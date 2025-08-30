87c7498c713f43dbed84eaefe65154c24f0b9c2e
Nichole Corpron <215328826+ncorpron@users.noreply.github.com>
2025-08-29 21:09:25 -0400

    Update reports.py

--- reports.py
+++ reports.py
@@ -1,9 +1,15 @@
 import streamlit as st
-from utils import load_data
+from utils import load_data, recalc
 import io
 
 def reports_page():
-    st.title("Reports Page")
-    inventory = load_data()
-    csv = inventory.to_csv(index=False).encode('utf-8')
-    st.download_button("Download CSV", data=csv, file_name="inventory.csv")
+    st.title("📄 Reports")
+    inventory, usage_df, months = load_data()
+    filtered_inventory = recalc(inventory, months)
+
+    csv = filtered_inventory.to_csv(index=False).encode('utf-8')
+    st.download_button("⬇️ Download CSV", data=csv, file_name="inventory.csv")
+
+    output = io.BytesIO()
+    filtered_inventory.to_excel(output, index=False, engine='openpyxl')
+    st.download_button("⬇️ Download Excel", data=output.getvalue(), file_name="inventory.xlsx")
