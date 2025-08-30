4cd4fb06ec6786692f23d96a218c8b01be3ff553
Nichole Corpron <215328826+ncorpron@users.noreply.github.com>
2025-08-30 08:27:25 -0400

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-def show_banner():
+def show_banner(image_url):
     st.markdown(
-        """
+        f"""
         <style>
-        .food-banner {
-            background-image: url('https://images.unsplash.com/photo-1504674900247-0877df9cc836');
+        .food-banner {{
+            background-image: url('{image_url}');
             background-size: cover;
             background-position: center;
             height: 220px;
             border-radius: 12px;
             margin-bottom: 20px;
             box-shadow: 0 4px 12px rgba(0,0,0,0.3);
-        }
+        }}
         </style>
         <div class="food-banner"></div>
         """,
